4f8de94229cb28fc3ee0cf3e08a8678fdf9bd84d
aborashed94 <m.al.omar94@gmail.com>
2022-04-25 21:18:10 +0400

    Add overlay state to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,22 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    overlay: false,
   },
   getters: {
+    getOverlay: (state) => state.overlay,
   },
   mutations: {
+    setOverlay: (state, payload) => (state.overlay = payload),
   },
   actions: {
+    setOverlay: (context, payload) => {
+      context.commit("setOverlay", payload);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
