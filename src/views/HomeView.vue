376115f7c2fcc25b5ea107eabc34314a3e60c47e
aborashed94 <m.al.omar94@gmail.com>
2022-04-26 23:50:33 +0400

    Update home view with all charts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,39 @@
 <template>
-  <hello-world />
+  <v-container class="dashboard">
+    <v-row>
+      <v-col>
+        <v-card> <line-chart /></v-card>
+      </v-col>
+      <v-col>
+        <v-card><bar-chart /> </v-card>
+      </v-col>
+      <v-col>
+        <v-card><area-chart /> </v-card>
+      </v-col>
+      <v-col>
+        <v-card><pie-chart /> </v-card>
+      </v-col>
+      <v-col>
+        <v-card><radar-chart /> </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
+import LineChart from "../components/chartjs/LineChart.vue";
+import BarChart from "../components/chartjs/BarChart.vue";
+import AreaChart from "../components/chartjs/AreaChart.vue";
+import PieChart from "../components/chartjs/PieChart.vue";
+import RadarChart from "../components/chartjs/RadarChart.vue";
 export default {
-  name: "HomeView",
-
+  name: "DashboardView",
   components: {
-    HelloWorld,
+    LineChart,
+    BarChart,
+    AreaChart,
+    PieChart,
+    RadarChart,
   },
 };
 </script>
