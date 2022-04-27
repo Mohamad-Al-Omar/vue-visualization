b0708aa96747e8662ab7da388bcf705cbd5a7a1c
aborashed94 <m.al.omar94@gmail.com>
2022-04-27 22:15:10 +0400

    Move chart data and options to parent

--- src/components/chartjs/LineChart.vue
+++ src/components/chartjs/LineChart.vue
@@ -3,37 +3,18 @@ import { Line } from "vue-chartjs";
 
 export default {
   extends: Line,
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+    chartOptions: {
+      type: Object,
+      required: true,
+    },
+  },
   mounted() {
-    this.renderChart(
-      {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-        ],
-        datasets: [
-          {
-            label: "Data 1",
-            data: [2, 10, 5, 9, 0, 6, 20],
-            backgroundColor: "transparent",
-            borderColor: "rgba(1, 116, 188, 0.50)",
-            pointBackgroundColor: "rgba(171, 71, 188, 1)",
-          },
-        ],
-      },
-      {
-        responsive: true,
-        maintainAspectRatio: false,
-        title: {
-          display: true,
-          text: "My Data",
-        },
-      }
-    );
+    this.renderChart(this.chartData, this.chartOptions);
   },
 };
 </script>
