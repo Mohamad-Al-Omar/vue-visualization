97500e82568db11d5b157062c160eb93a3754ef6
aborashed94 <m.al.omar94@gmail.com>
2022-04-27 22:14:42 +0400

    Create doughnut chart

---
+++ src/components/chartjs/DoughnutChart.vue
@@ -0,0 +1,15 @@
+<script>
+import { Doughnut, mixins } from "vue-chartjs";
+
+export default {
+  name: "DoughnutChart",
+  extends: Doughnut,
+  mixins: [mixins.reactiveProp],
+  props: ["chartData", "chartOptions"],
+  mounted() {
+    this.renderChart(this.chartData, this.chartOptions);
+  },
+};
+</script>
+
+<style></style>
