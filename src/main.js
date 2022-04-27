08f93b0f9bf698abba188cfda8419d847f03730e
aborashed94 <m.al.omar94@gmail.com>
2022-04-27 22:14:27 +0400

    Add funtions mixin

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-
+import "./mixins/functions";
 Vue.config.productionTip = false;
 
 new Vue({
+  // mixins: [functions],
   router,
   store,
   vuetify,
