6af07db2cb08980ae3ffb7049415efa8c4c483ab
aborashed94 <m.al.omar94@gmail.com>
2022-05-14 03:22:45 +0400

    fix change project name

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-dashboard/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/vue-visualization/" : "/",
   transpileDependencies: ["vuetify"],
   css: {
     loaderOptions: {
